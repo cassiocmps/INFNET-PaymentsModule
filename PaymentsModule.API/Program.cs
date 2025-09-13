4b22296f41808155e0db5ce2cc16cb0804827cbe
cassiocmps <cassio.szcampos@gmail.com>
2025-09-13 13:47:43 -0300

    Add swagger

--- PaymentsModule.API/Program.cs
+++ PaymentsModule.API/Program.cs
@@ -7,12 +7,19 @@ namespace PaymentsModule.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            // Register Swagger/OpenAPI services
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.UseHttpsRedirection();
 
